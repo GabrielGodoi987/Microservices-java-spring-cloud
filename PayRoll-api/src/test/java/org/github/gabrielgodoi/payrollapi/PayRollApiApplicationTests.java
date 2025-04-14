afff80e3db64525b0bd79b4889fd2b03aebe744e
GabrielGodoi452 <gabrielgodoi452@gmail.com>
2025-04-14 10:29:10 -0300

    second microservice

---
+++ PayRoll-api/src/test/java/org/github/gabrielgodoi/payrollapi/PayRollApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.gabrielgodoi.payrollapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayRollApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
